90eeeae55b6ddc436147bf6f1f9d9f1c36bbc0d1
juniormusasizi61 <juniormusasizi6@gmail.com>
2026-02-01 11:53:15 +0300

    login authentication routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout";
+import RequireAuth from "./components/common/RequireAuth";
 
 import Chat from "./pages/Chat";
 import ReadBible from "./pages/ReadBible";
 import Saved from "./pages/Saved";
 import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
+        <Route element={<RequireAuth />}>
         <Route index element={<Chat />} />
         <Route path="read-bible" element={<ReadBible />} />
         <Route path="saved" element={<Saved />} />
         <Route path="profile" element={<Profile />} />
       </Route>
+      </Route>
+
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
